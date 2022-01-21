f1300e45ac595696e93fcb7fd24fb6fb0c2fbb2e
ys-sh <yoshida@ys-sh.jp>
2022-01-21 11:45:39 +0900

    cmap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-
+import jp_JP from '@react-pdf-viewer/locales/lib/jp_JP.json';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
@@ -11,6 +11,7 @@ const App = () => {
     const [url, setUrl] =  React.useState("https://ys-sh.github.io/pdf-viewer-starter/test.pdf");
     
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    const cMap = { url: "https://ys-sh.github.io/cmaps/", isCompressed: true }
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.js">
             <div
@@ -24,6 +25,9 @@ const App = () => {
                 <Viewer
                     fileUrl={url}
                     plugins={[defaultLayoutPluginInstance]}
+                    localization={jp_JP}
+                    characterMap={cMap}
+
                 />
             </div>
         </Worker>
